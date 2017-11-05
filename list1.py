9c34da1ae1c4d5acaa4ac8b35416267a2f8bb2e4
Russell Williams <Russ@RussWill.com>
2017-11-05 13:09:48 -0600

    done with lambda

--- list1.py
+++ list1.py
@@ -54,12 +54,9 @@ def front_x(words):
 # e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
 # [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 # Hint: use a custom key= function to extract the last element form each tuple.
-def skey(tups):
-  return tups[1]
-
 def sort_last(tuples):
   # +++your code here+++
-  return sorted(tuples, key = skey)
+  return sorted(tuples, key = lambda thing: thing[1])
 
 
 # Simple provided test() function used in main() to print
